08d83c8b9c245bc33c33e9ac561da8ced1b7b86b
tianqi <tq85032@gmail.com>
2023-11-07 09:54:37 +0300

    module5-task3

--- frontend/src/stores/DataStore.js
+++ frontend/src/stores/DataStore.js
@@ -1,5 +1,9 @@
 import { defineStore } from "pinia";
-
+import dough from "../mocks/dough.json"
+import ingredients from "../mocks/ingredients.json"
+import misc from "../mocks/misc.json"
+import sauce from "../mocks/sauces.json"
+import size from "../mocks/sizes.json"
 export const useDataStore = defineStore('data',{
     state:()=>({
         dough:[],
@@ -10,7 +14,14 @@ export const useDataStore = defineStore('data',{
     }),
     getters:{
 
-    },actions:{
-        
+    },
+    actions:{
+        async initData(){
+            this.dough = JSON.parse([...dough]) 
+            this.ingredients =JSON.parse([...ingredients])
+            this.misc = JSON.parse([...misc])
+            this.sauce = JSON.parse([...sauce])
+            this.sizes = JSON.parse([...this.sizes])
+        },
     }
 })
--- frontend/src/stores/PizzaStore.js
+++ frontend/src/stores/PizzaStore.js
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+import {useDataStore} from './DataStore'
 
 export const usePizzaStore = defineStore('pizza',{
     state:()=>({
@@ -9,6 +10,20 @@ export const usePizzaStore = defineStore('pizza',{
         ingredients:[]
     }),
     getters:{
+        getAmount(state){
+            const dataStore = useDataStore()
+            dataStore.initData();
+            let amount= 0
+            let amountIngredients = 0
+            let amountSauce = (dataStore.sauce.find(element=>{element.id === state.sauceId})).price
+            let amountdough = (dataStore.dough.find(element=>{element.id === state.doughId})).price
+            let size = (dataStore.sizes.find(element=>{element.id === state.sizeId})).multiplier
+            state.ingredients.map(e=>{
+                amountIngredients +=(dataStore.ingredients.find(element=>{element.id === e.ingredientId})).price * e.quantity
+            })
+            amount = amountdough*size + amountSauce + amountIngredients
+            return amount
+        }
 
     },
     actions:{
