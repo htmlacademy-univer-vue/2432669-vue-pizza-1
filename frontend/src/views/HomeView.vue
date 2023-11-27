199514ebd3f9766753c05900f2c1ee13b35c4904
tianqi <tq85032@gmail.com>
2023-11-27 22:55:26 +0300

    move-change

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -56,11 +56,11 @@ let show = computed(()=>{
 function moveTask($event,task){
   let obj = JSON.parse($event)
   let id = obj.id
-  if(data.ingredients[id-1]!== undefined){
+  if(data.ingredients[id-1]!== undefined&& data.ingredients[id-1].count<3){
      data.ingredients[id-1].count = parseInt(data.ingredients[id-1].count) + 1
      data.ingredients[id-1].amount += parseInt( data.ingredients[id-1].amount)+ obj.price
     
-  }else{
+  }else if(data.ingredients[id-1]=== undefined){
     data.ingredients[id-1]= {amount:obj.price,count:1,id:id,name:obj.name}
 
   }
